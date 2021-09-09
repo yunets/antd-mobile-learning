6501d414c808eb21a0451e689eac40bf54e3c391
yunets <724547564@qq.com>
2021-09-09 11:24:19 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Steps } from "antd-mobile";
+import {
+  CheckCircleFilled,
+  ClockCircleFilled,
+  DollarCircleFilled
+} from "@ant-design/icons";
+const { Step } = Steps;
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>我说了无脑多，你说钢铁煤炭大涨了不</h1>
+      <hr />
+
+      <Steps direction="vertical">
+        <Step
+          title="填写机构信息"
+          status="finish"
+          description="完成时间：2020-12-01 12:30"
+        />
+        <Step
+          title="签约机构"
+          status="finish"
+          description="完成时间：2020-12-01 12:30"
+        />
+        <Step
+          title="关联服务区"
+          status="finish"
+          description="完成时间：2020-12-01 12:30"
+        />
+        <Step title="审批失败" status="error" />
+      </Steps>
+    </div>
+  );
+}
