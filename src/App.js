eb33d118c9cef4c352e429557f611bf1496ba82e
yunets <724547564@qq.com>
2021-09-10 14:24:56 +0800

    test test

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {
   ClockCircleFilled,
   DollarCircleFilled
 } from "@ant-design/icons";
+import { MyNav } from "./page/nav/MyNav";
 const { Step } = Steps;
 
 export default function App() {
   return (
     <div className="App">
+      <MyNav />
       <h1>this is a test !!</h1>
       <hr />
 
---
+++ src/page/nav/MyNav.js
@@ -0,0 +1,10 @@
+import React from "react";
+export class MyNav extends React.Component {
+  render() {
+    return (
+      <div>
+        <legend>this is myhao123</legend>
+      </div>
+    );
+  }
+}
