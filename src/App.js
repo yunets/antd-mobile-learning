9538bbb34c7a92590c1d84f5c06a7d1b3c149978
yunets <724547564@qq.com>
2021-12-09 11:30:41 +0800

    userinfo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { School } from "./page/info/School";
+import { UserInfo } from "./page/info/UserInfo";
 
 export default function App() {
   return (
     <div className="App">
       <h1>this is a test !!</h1>
+      <UserInfo />
       <School />
     </div>
   );
