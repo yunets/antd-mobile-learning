b8cbd64df4e7467891de6873cd53019dcb0112b3
yunets <724547564@qq.com>
2021-12-11 10:14:12 +0800

    意向

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { UserInfo } from "./page/info/UserInfo";
 export default function App() {
   return (
     <div className="App">
-      <h1>this is a test !!</h1>
-      <TechnologyStack />
+      <h1>意向岗位为高级JAVA工程师、大数据开发工程师、全栈工程师</h1>
       <UserInfo />
       <School />
+      <TechnologyStack />
       <ProjectHistory />
       <BackTop />
     </div>
